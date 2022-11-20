cf525fee87edb1c442c0b2ff21df511d8072cc62
ozuaq <oza4456@gmail.com>
2022-11-20 15:26:16 +0900

    temp

--- mysite/views.py
+++ mysite/views.py
@@ -3,9 +3,16 @@ from django.contrib import messages
 from django.http import HttpResponseRedirect
 from django.urls import reverse
 
+from .models import SpotInfo
+
 # Create your views here.
 def zoom_out_map_view(request):
-    return render(request, 'mysite/zoom_out_map.html')
+    pin_list = []
+    
+    pins = SpotInfo.objects.all()
+    for pin in pins:
+        pin_list.append({'id': pin.id, 'name': pin.location_name, 'category': pin.category, 'time': pin.time, 'season': pin.season, 'tags': pin.tags, 'image': pin.image, 'appeal': pin.appeal, 'good': pin.good, 'visit': pin.visit, 'x': pin.position_x, 'y': pin.position_y})
+    return render(request, 'mysite/zoom_out_map.html', {'pin_list': pin_list})
 
 def modal_test(request):
     return render(request, 'mysite/modal_test.html')
